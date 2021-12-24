9f69e1eec3f6c6c981101267802eda58ffee74db
paradoksalno <paradoksalnoportal@gmail.com>
2021-12-24 20:42:52 +0100

    site info update

--- src/_data/site.js
+++ src/_data/site.js
@@ -1,9 +1,9 @@
 module.exports = {
-  title: 'Vredeburg',
-  description: 'Eleventy starter project to create a blog',
+  title: 'Paradoksalno',
+  description: 'Hrvatski portal',
   keywords: ['eleventy', 'template', 'simple', 'clean'],
   // your site url without trailing slash
-  url: 'https://vredeburg.netlify.app',
+  url: 'https://paradoksalno.com',
   // how many posts you want to show for each page
   paginate: 6
   // if you want to add disqus to your site
